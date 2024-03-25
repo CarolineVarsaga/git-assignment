b42a37e336d1665272e8244620233e4144f8595f
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-03-25 16:15:03 +0100

    Adds slide script

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,50 @@
+let slideIndex = 1;
+    showSlides(slideIndex);
 
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        const slides = document.getElementsByClassName("slides");
+        const dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none"; 
+        }
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].classList.remove("active"); 
+        }
+        if (dots[slideIndex-1]) {
+            slides[slideIndex-1].style.display = "block"; 
+            dots[slideIndex-1].classList.add("active"); 
+        }
+    }
+    
+
+    setInterval(function() {
+        plusSlides(1);
+    }, 8000);
+
+    const prev = document.querySelector('.prev');
+    const next = document.querySelector('.next');
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+    
+    const dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].addEventListener('click', function() {
+            currentSlide(Array.prototype.indexOf.call(dots, this) + 1);
+        });
+    }
